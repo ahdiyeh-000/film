78a0fc067667b67b3530f073ad0afa4aad0188a8
Rahimi PC <Rahimi PC@RAHIMI>
2017-08-12 11:52:03 +0430

    1

--- DigiMovie/DigiMovie/Controllers/HomeController.cs
+++ DigiMovie/DigiMovie/Controllers/HomeController.cs
@@ -3,35 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Net;
-
 
 namespace DigiMovie.Controllers
 {
     public class HomeController : Controller
-
-
-
     {
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About(int? id,string message)
+        public ActionResult About()
         {
-            // ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.";
 
-             return View();
-        //    if (id==null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-
-        //    }
-        //    return Content(id.ToString()+"<hr>"+ message);
-       
-       }
+            return View();
+        }
 
         public ActionResult Contact()
         {
