c937385eb1c4ef02a3000377302dcd394b4c20cc
Rahimi PC <Rahimi PC@RAHIMI>
2017-08-12 11:52:03 +0430

    1

---
+++ DigiMovie/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+
+namespace DigiMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int? id,string message)
+        {
+            // ViewBag.Message = "Your application description page.";
+
+             return View();
+        //    if (id==null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+
+        //    }
+        //    return Content(id.ToString()+"<hr>"+ message);
+       
+       }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DigiMovie/Controllers/TempController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiMovie.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Temp
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        [ Route("Gallery/{year:int range(1300,1400)}/{month:int:range(1,12)}/{message:maxlength(5)?}")]
+        public ActionResult ShowAlbum(string year,string month,string message)
+        {
+            return Content(year + "<hr>" + month + "<hr>" +message);
+        }
+
+
+    }
+
+
+}
